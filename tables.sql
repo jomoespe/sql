d0f6258b4c090c17d710c21f2215c2ed92fb87ec
jose <jomoespe@gmail.com>
2020-05-26 19:50:02 +0200

    Fix errors in SQL commands

--- tables.sql
+++ tables.sql
@@ -1,30 +1,33 @@
 --
 -- DDL
 --
-CREATE TABLE department
+CREATE TABLE IF NOT EXISTS departments
 (
     DepartmentID   INT         PRIMARY KEY,
     DepartmentName VARCHAR(20)
 );
 
 
-CREATE TABLE employees
+CREATE TABLE IF NOT EXISTS employees
 (
-    FistName     VARCHAR(20),
+    FirstName    VARCHAR(20),
     DepartmentID INT         REFERENCES departments(DepartmentID)
 );
 
 --
 -- DML
 --
+DELETE FROM employees;
+DELETE FROM departments;
+
 INSERT INTO departments (DepartmentID, DepartmentName) VALUES (31, 'Ventas');
 INSERT INTO departments (DepartmentID, DepartmentName) VALUES (33, 'Ingeniería');
 INSERT INTO departments (DepartmentID, DepartmentName) VALUES (34, 'Contabilidad');
 INSERT INTO departments (DepartmentID, DepartmentName) VALUES (35, 'Marketing');
 
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Rafael', 31);
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Juan', 33);
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Hernando', 33);
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Roberto', 34);
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Pedro', 34);
-INSERT INTO employees (FirstName, DepartmentName) VALUES ('Guillermo', NULL);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Rafael', 31);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Juan', 33);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Hernando', 33);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Roberto', 34);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Pedro', 34);
+INSERT INTO employees (FirstName, DepartmentID) VALUES ('Guillermo', NULL);
